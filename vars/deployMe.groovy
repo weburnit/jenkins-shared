3a885e17b708ccd497376b24a7b6723830897636
paul_nguyen <paul_nguyen@mckinsey.com>
2021-08-05 14:42:41 +0700

    move variable

--- vars/deployMe.groovy
+++ vars/deployMe.groovy
@@ -46,8 +46,6 @@ def call(Map pipelineParams) {
                     docker.withRegistry( '', registryCredential ) {
                         docsImage.push()
                     }
-                    echo serviceName
-                    echo "$serviceName"
                     sh "helm upgrade --install ${serviceName}-release-notes ${helmRepo}/${helmReleaseNote} --set image.repository=${registry}-release-notes --set image.tag=$BUILD_TAG --set fullnameOverride=${serviceName}-release-notes"
 
                     sh "docker rmi ${registry}-release-notes:${env.BUILD_TAG}"
