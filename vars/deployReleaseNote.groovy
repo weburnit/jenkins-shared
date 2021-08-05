3cc133bc512e8c5fd1d5651e998546654accdee6
paul_nguyen <paul_nguyen@mckinsey.com>
2021-08-05 10:11:42 +0700

    [main] Pipeline params for Release notes

--- vars/deployReleaseNote.groovy
+++ vars/deployReleaseNote.groovy
@@ -1,13 +1,11 @@
 def call(Map pipelineParams) {
     pipeline {
       environment {
-        registry = "weburnit/hello-world"
-        registryCredential = 'weburnit'
-        dockerImage = ''
-        helmReleaseNoteRegistry = 'weburnit/hello-world-release-notes'
+        registry = pipelineParams.registry
+        registryCredential = pipelineParams.registryCredential
+        helmRepo = pipelineParams.helmRepo
+        serviceName = pipelineParams.serviceName
         helmReleaseNote = 'release-notes'
-        helmRepo = 'lancar-helm-museum'
-        helmPackage = 'go-base'
       }
       agent any
       stages {
@@ -16,27 +14,11 @@ def call(Map pipelineParams) {
             git branch: 'main', url: 'https://github.com/weburnit/hello-world.git'
           }
         }
-        stage('Building image') {
-          steps{
-            script {
-              dockerImage = docker.build registry + ":$BUILD_TAG"
-            }
-          }
-        }
-        stage('Deploy Image') {
-          steps{
-            script {
-              docker.withRegistry( '', registryCredential ) {
-                dockerImage.push()
-              }
-            }
-          }
-        }
         stage('Building Release Notes image') {
           steps{
             script {
                 def dockerfile = 'Docs.Dockerfile'
-                def docsImage = docker.build("${helmReleaseNoteRegistry}:${env.BUILD_TAG}", "-f ${dockerfile} .")
+                def docsImage = docker.build("${registry}-release-notes:${env.BUILD_TAG}", "-f ${dockerfile} .")
 
                 docker.withRegistry( '', registryCredential ) {
                     docsImage.push()
@@ -44,23 +26,16 @@ def call(Map pipelineParams) {
             }
           }
         }
-        stage('Remove Unused docker image') {
-          steps{
-            sh "docker rmi $registry:$BUILD_TAG"
-          }
-        }
-        stage('Update Helm') {
-          steps{
-              sh '''
-              helm upgrade --install ${SERVICE_NAME} $helmRepo/$helmPackage --set image.repository=$registry --set image.tag=$BUILD_TAG --set fullnameOverride=${SERVICE_NAME}-golang
-              '''
-          }
-        }
         stage('Update Helm Release Note service') {
           steps{
               sh '''
-              helm upgrade --install ${SERVICE_NAME}-release-notes $helmRepo/$helmReleaseNote --set image.repository=${helmReleaseNoteRegistry} --set image.tag=$BUILD_TAG --set fullnameOverride=${SERVICE_NAME}-release-notes
+              helm upgrade --install ${SERVICE_NAME}-release-notes $helmRepo/$helmReleaseNote --set image.repository=${registry}-release-notes --set image.tag=$BUILD_TAG --set fullnameOverride=${serviceName}-release-notes
               '''
+          }
+        }
+        stage('Remove Unused docker image') {
+          steps{
+            sh "docker rmi ${registry}-release-notes:${env.BUILD_TAG}
           }
         }
       }
